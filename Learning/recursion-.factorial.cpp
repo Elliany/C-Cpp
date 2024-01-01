cc23508369fdd15dd60a9dff37911b5ce5b90368
Dani Nguyen <140548128+Elliany@users.noreply.github.com>
2024-01-01 21:58:53 +0700

    C-Cpp

--- Learning/recursion-.factorial.cpp
+++ Learning/recursion-.factorial.cpp
@@ -1,27 +1,50 @@
 #include <stdio.h>
 
 long int factorial(int n) {
-    long f = 1;
-    int i;
-    for (i = 1; i <= n; i++)
+    if (n < 0) return -1; // Factorial not defined for negative numbers
+    if (n == 0) return 1; // 0! is 1
+
+    long int f = 1;
+    for (int i = 1; i <= n; i++)
         f = f * i;
     return f;
 }
 
 long int recursive_factorial(int n) {
-    if (n == 1)
-        return 1;
-    else 
-        return n * recursive_factorial(n - 1);
+    if (n < 0) return -1; // Factorial not defined for negative numbers
+    if (n == 0) return 1; // 0! is 1
+
+    return n * recursive_factorial(n - 1);
 }
 
 int main() {
-    int how_many = 0, i;
+    int how_many = 0;
     printf("How many numbers do you want to factorial? ");
     scanf("%d", &how_many);
-    printf("\nFactorial\n");
-    for (i = 1; i <= how_many; i++)
-        printf("\n%d \t %ld\n", i, factorial(i));
+
+    if (how_many < 0) {
+        printf("Negative number entered. Exiting program.\n");
+        return 1; // Exit with error code for negative number
+    }
+
+    printf("\nIterative Factorial\n");
+    for (int i = 0; i <= how_many; i++) {
+        long int result = factorial(i);
+        if (result != -1)
+            printf("\n%d \t %ld\n", i, result);
+        else
+            printf("\n%d \t Undefined (Negative Number)\n", i);
+    }
+    printf("\n\n");
+
+    printf("Recursive Factorial\n");
+    for (int i = 0; i <= how_many; i++) {
+        long int result = recursive_factorial(i);
+        if (result != -1)
+            printf("\n%d \t %ld\n", i, result);
+        else
+            printf("\n%d \t Undefined (Negative Number)\n", i);
+    }
     printf("\n\n");
     return 0;
 }
